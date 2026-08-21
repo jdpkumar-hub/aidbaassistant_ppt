75e9dbe873ce6a16370abf61ac9dc4c2a098d33b
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-08-21 15:19:50 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Read the HTML file
+with open('your_html_file.html', 'r') as file:
+    html_content = file.read()
+
+# Render the HTML
+components.html(html_content, height=600)
