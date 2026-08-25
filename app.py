d5c0867cf5ebea2f5cb680cd8a8925343d50be2f
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-08-24 23:29:39 -0500

    Change HTML file name from 'your_html_file' to 'index'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Read the HTML file
-with open('your_html_file.html', 'r') as file:
+with open('index.html', 'r') as file:
     html_content = file.read()
 
 # Render the HTML
